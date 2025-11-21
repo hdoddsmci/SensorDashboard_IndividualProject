96c630fccdec3cc697aab2c6f3d3ec87e5ead385
hamish <hamishmcintoshh@gmail.com>
2025-11-22 03:10:31 +1300

    feat: Initial project structure and basic Sensor class

---
+++ SensorDashboard/VirtualSensor/Sensor.cs
@@ -0,0 +1,20 @@
+﻿namespace VirtualSensor
+{
+    public class Sensor
+    {
+        public string Name { get; set; }
+        public string Location { get; set; }
+        public double MinValue { get; set; }
+        public double MaxValue { get; set; }
+
+        // This matches your assignment requirement: "InitialiseSensor"
+        public void InitialiseSensor(string name, string location, double min, double max)
+        {
+            // TODO: Add validation here later
+            Name = name;
+            Location = location;
+            MinValue = min;
+            MaxValue = max;
+        }
+    }
+}
